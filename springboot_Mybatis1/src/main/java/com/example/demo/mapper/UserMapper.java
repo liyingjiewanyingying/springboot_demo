56e92b70563e793430d78581c465c567ccbc7695
liyingjiewanyingying <1044209998@qq.com>
2020-04-08 21:35:51 +0800

    新建springboot_Mybatis

---
+++ springboot_Mybatis1/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.mapper;
+
+import com.example.demo.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("select * from users")
+    public List<User> findAll();
+}
---
+++ springboot_Mybatis1/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> findAll();
+}
